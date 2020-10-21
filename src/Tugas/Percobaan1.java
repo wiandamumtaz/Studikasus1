430a9ea3d93a594692a6b5109615609280b665df
wiandamumtaz <wianda_mumtaz_29rpl@student.smktelkom-mlg.com>
2020-10-21 12:27:06 +0700

    Selamat, Kamu Berhasil

---
+++ src/Tugas/Percobaan1.java
@@ -0,0 +1,22 @@
+package Tugas;
+
+public class Percobaan1 {
+
+    public static void main(String[] args) {
+
+      String Nama,Nisn,TanggalLahir,Kelamin;
+      
+      Nama = "Wianda Mumtaz";
+      Nisn = "0049873754";
+      TanggalLahir = "20-04-2004";
+      Kelamin = "Perempuan";
+      
+        System.out.println("Kartu Pelajar");  
+        System.out.println("Nama :"+ Nama);
+        System.out.println("Nisn :"+ Nisn);
+        System.out.println("Tanggal Lahir :"+ TanggalLahir);
+        System.out.println("Kelamin :"+ Kelamin);
+   
+    }
+    
+}
